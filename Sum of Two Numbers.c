d272c46cfc77293c113c9912306caa7d845a563f
Liptan Adhikari <110087917+LiptaNAdhikari@users.noreply.github.com>
2023-08-23 00:35:29 +0600

    Add files via upload

---
+++ Sum of Two Numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int n1,n2,sum;
+    printf("Enter two number: \n");
+    scanf("%d %d", &n1, &n2);
+    sum = n1+n2;
+    printf("Total is: %d",sum);
+return 0;
+}
+
